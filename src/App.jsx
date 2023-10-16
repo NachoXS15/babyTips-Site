9f45d098aee4b4f76e588ccf5167c37d6eae0635
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-10-16 20:16:59 -0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { React, useState } from 'react'
+import "bootswatch/dist/morph/bootstrap.min.css"
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  <Header />
+}
+
+export default App
