dba57ac381120a5ae2704a657c96b1f20aac94d3
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-10-16 21:16:33 -0300

    created v1 header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { React, useState } from 'react'
-import "bootswatch/dist/morph/bootstrap.min.css"
-import './App.css'
-import Header from './components/Header'
+import { React, useState } from "react";
+import "bootswatch/dist/morph/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  <Header />
+  return(
+
+    <div>
+      <Header />;
+    </div>
+  )
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
+import React from 'react'
+import BabyTipsLogo from "../assets/babytips-logo.png"
 export default function Header() {
-    return (
-        
-    );
+  return (
+    <>
+        <div className='p-3 bg-primary'>
+            <h3 className='text-center'>Descuento disponible por las proximas 48hs</h3>
+        </div>
+        <nav className='p-3 navbar navbar-expanded-lg bg-secondary d-flex justify-content-around align-items-center'>
+            <div className='d-flex justify-content-around align-items-center'>
+                <img src={BabyTipsLogo} width={80} />
+                <h3 className='d-inline'>Baby Tips</h3>
+            </div>
+            <div>
+                <a className='m-2' style={{textDecoration: 'none'}}>¿Qué vendemos?</a>
+                <a className='m-2' style={{textDecoration: 'none'}}>¿Cómo comprar?</a>
+                <a className='m-2' style={{textDecoration: 'none'}}>Contactanos</a>
+            </div>
+        </nav>
+    </>
+  )
 }
